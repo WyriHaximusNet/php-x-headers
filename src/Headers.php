30a8baa668e73bca7edb107be7d3d18e63c075fd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-09-18 23:05:35 +0200

    Next gen

--- src/Headers.php
+++ src/Headers.php
@@ -1,22 +1,39 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WyriHaximusNet\XHeaders;
 
+use function array_rand;
+use function is_string;
+
 final class Headers
 {
-    public static function headers(): array
+    public const HEADERS = [
+        'X-nananana' => 'Batcache',
+        'X-Wars' => 'May the Force be with us',
+        'X-Trek' => 'Live long and prosper',
+        'X-Gate' => 'Jaffa! Kree!',
+        'X-Oniell' => 'In the middle of my backswing!',
+        'X-Tealc' => 'Indeed',
+        'X-Anna' => 'Do you want to build a snowman?',
+        'X-Olaf' => 'I love summer!',
+        'X-Wing' => 'Pew pew',
+        'X-ClacksOverhead' => 'GNU Terry Pratchett',
+    ];
+
+    /**
+     * @return iterable<string, string>
+     */
+    public static function random(int $count): iterable
     {
-        return [
-            'X-nananana' => 'Batcache',
-            'X-Wars' => 'May the Force be with us',
-            'X-Trek' => 'Live long and prosper',
-            'X-Gate' => 'Jaffa! Kree!',
-            'X-Oniell' => 'In the middle of my backswing!',
-            'X-Tealc' => 'Indeed',
-            'X-Anna' => 'Do you want to build a snowman?',
-            'X-Olaf' => 'I love summer!',
-            'X-Wing' => 'Pew pew',
-            'X-ClacksOverhead' => 'GNU Terry Pratchett',
-        ];
+        $keys = array_rand(self::HEADERS, $count);
+        if (is_string($keys)) {
+            $keys = [$keys];
+        }
+
+        foreach ($keys as $key) {
+            yield $key => self::HEADERS[$key];
+        }
     }
 }
